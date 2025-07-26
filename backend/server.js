9fd78bc4a57a50ee36a8b70dfb48911b07105a97
Avnish Srivastava <srivastavavicks@gmail.com>
2025-07-26 17:54:19 +0530

    made backend for real estate

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+dotenv.config();
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/properties",require("./routes/propertydetails"))//property details show karne ke liye
+app.use("/api/propertydetails",require("./routes/propertydetailsdynamic"))//koi specific property show karna kr liye
+app.use("/api/rentprop",require("./routes/rentprop"))//for rental property details
+app.use("/api/rentpropdetails",require("./routes/rentpropdynamic"))//for dynmaic rent prop details
+app.use("/api/emptyplotdetails",require("./routes/emptyplots"))//for details of empty plots
+app.use("/api/plotdetailsdynamic",require("./routes/emptyplotsdynamic"))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} `);
+});
